177bf72a88015abad66e0f3801ef69dfd317e2ca
MrCorncub <ebner.bernhard98@gmx.at>
2022-09-08 14:40:53 +0200

    ad

--- src/html.js
+++ src/html.js
@@ -12,7 +12,6 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <meta name="propeller" content="35a7b3b1fe48af9a9a87d8dae578c6a3"/>
-        <script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://inklinkor.com/tag.min.js',5367070,document.body||document.documentElement)</script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
